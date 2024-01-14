9c4aab4e1fab8c029ebe5326accc24c83a2edef0
Anoop Kulkarni <anoopkulkarni1807.com>
2024-01-14 17:39:27 +0530

    initial commit

---
+++ src/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar  from "../components/Navbar";
+import Footer from '../components/Footer';
+
+export default function Header() {
+  return (
+    <div>
+      <div><Navbar></Navbar></div>
+      <div>Body</div>
+      <div><Footer/></div>
+    </div>
+  )
+}
